fa4d76c05bb782a4bf01465d508e5a0a4a5d935d
priyanshnama <priyanshnama@gmail.com>
2020-02-09 18:45:39 +0530

    initial commit

--- app/src/main/java/com/priyanshnama/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/priyanshnama/tic_tac_toe/MainActivity.kt
@@ -2,11 +2,21 @@ package com.priyanshnama.tic_tac_toe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun O(View:View){
+        Log.i("idadn","O is clicked")
+    }
+
+    fun X(View:View){
+        Log.i("idadn","X is clicked")
     }
 }
