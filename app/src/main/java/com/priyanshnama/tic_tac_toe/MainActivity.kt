f848de4fadd0952cd54692ace1ed960e43b99532
priyanshnama <priyanshnama@gmail.com>
2020-02-09 19:20:20 +0530

    initial commit

--- app/src/main/java/com/priyanshnama/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/priyanshnama/tic_tac_toe/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
